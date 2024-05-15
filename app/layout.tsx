e297baa234aa54b70101f58f0b7a8b3e0c9b995e
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-15 18:17:23 +0200

    added custom layout to admin page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import NavBar from "./NavBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="winter">
+      <body className={inter.className}>
+        <NavBar/>
+          <main className="p-5"> {children}</main>
+       
+        </body>
     </html>
   );
 }
