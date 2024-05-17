1fe06339acd896d0c518b13c682f147c2f72e7a4
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-17 23:44:50 +0200

    added prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,27 @@
 import NextAuth from "next-auth/next";
 import  GoogleProvider from 'next-auth/providers/google'
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
 
-export const authOptions={
+
+const prisma= new PrismaClient()
+export const authOptions:NextAuthOptions ={
+    adapter:PrismaAdapter(prisma),
     providers:[
         GoogleProvider({
             clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
             clientId:process.env.GOOGLE_CLIENT_ID!,
              httpOptions:{
                 timeout:15000
-             }
+             },
+             
         
         })
     ],
+    session:{
+        strategy: "jwt"
+    },
     secret:process.env.NEXT_AUTH_KEY,
     debug:true
 }
