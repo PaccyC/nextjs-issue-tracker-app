f32775acc95c3210f1c569770680dc66b46798e4
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-18 17:56:34 +0200

    showing uploaded files from cloudinary

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,14 +1,44 @@
 "use client"
 
-import { CldUploadWidget } from 'next-cloudinary'
+import { CldUploadWidget,CldImage } from 'next-cloudinary'
+import { sources } from 'next/dist/compiled/webpack/webpack'
+import { useState } from 'react'
+
+interface CloudnaryResult{
+  public_id:string
+}
 const UploadPage = () => {
+  const [publicId,setPublicId]=useState('')
   return (
-    <CldUploadWidget uploadPreset='cgctjgtk'>
+    <>
+    {publicId &&
+     <CldImage src={publicId} width={270} height={180} alt=''/>
+    }
+    <CldUploadWidget 
+    options={
+      {
+        sources:["local"],
+        multiple:false,
+        maxFiles:5
+      }
+    }
+    onUploadAdded={(result,widget)=>{
+      
+        console.log(result);
+    if(result.event !== "success") return
+  
+
+    const info = result.info as CloudnaryResult
+     setPublicId(info.public_id)
+    }}
+    uploadPreset='cgctjgtk'>
         {({open})=><button 
         className='btn btn-primary'
         onClick={()=>open()}
         >Upload</button>}
     </CldUploadWidget>
+    </>
+    
   )
 }
 
