5c4d96a6a400f408d9b74028dc3b14b096cf55c9
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-18 15:17:31 +0200

    registering users using credentials

---
+++ app/api/register/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../users/schema";
+import prisma from "@/prisma/prisma";
+import bcrypt from 'bcrypt'
+export async function POST(request:NextRequest){
+    const body= await request.json();
+    
+    const user = await prisma.user.findUnique({
+        where:{
+            email:body.email
+        }
+    })
+    if(user){
+        return  NextResponse.json({error:"User already exists"},{status:400})
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword= await bcrypt.hash(body.password,salt)
+    const newUser= await prisma.user.create({
+        data:{
+            email:body.email,
+            hashedPassword
+        }
+    })
+    return NextResponse.json({email:newUser.email})
+
+}
--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 const schema= z.object({
-    name:z.string().min(3),
-    email:z.string().email()
+    email:z.string().email(),
+    password: z.string().min(5)
 })
 
 export default schema
