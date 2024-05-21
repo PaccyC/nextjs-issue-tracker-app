2cddb5c735eaed4c288b45656adf087df5939974
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-21 21:24:50 +0200

    built new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@radix-ui/themes"
+import Link from "next/link"
 const IssuesPage = () => {
   return (
     <div>
-      <Button >New Issue</Button>
+      <Button ><Link href="/issues/new">New issue</Link></Button>
     </div>
   )
 }
