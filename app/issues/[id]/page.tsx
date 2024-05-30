e9d6ac386afb0f456eb046c31d1b63baa8846e0c
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-30 18:05:36 +0200

    styling issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,20 +1,39 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/prisma'
-import { Flex, Heading } from '@radix-ui/themes'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-const SingleIssuePage = () => {
+interface Props{
+    params:{
+        id:string
+    }
+}
+
+const SingleIssuePage =async ({params:{id}}:Props) => {
     
     const issue= await prisma.issue.findUnique({where:{
-        id:parseInt(router.query.id)
+        id:parseInt(id)
     }})
+
+    if(!issue)
+        notFound();
   return (
     <div>
       
       <Heading>{issue.title}</Heading>
       <Flex className='space-x-3 my-2'>
-        
+      <IssueStatusBadge status={issue.status}/>
+      <Text>{issue.createdAt.toDateString()}</Text>
+      <p>{issue.status}</p>
 
       </Flex>
+      <Card>
+
+     {issue.desription}
+      </Card>
+        
+
 
     </div>
   )
